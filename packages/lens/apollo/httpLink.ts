97eaa50a59d2672bfcd3461557e226a322a0206a
Nikoemme.eth <94803063+nikomatt69@users.noreply.github.com>
2024-05-21 04:28:25 +0200

    Update httpLink.ts

--- packages/lens/apollo/httpLink.ts
+++ packages/lens/apollo/httpLink.ts
@@ -1,5 +1,6 @@
 import { HttpLink } from '@apollo/client';
 import { API_URL, APP_NAME } from '@lensshare/data/constants';
+import { setContext } from '@apollo/client/link/context';
 
 const httpLink = new HttpLink({
   uri: API_URL,
@@ -10,4 +11,17 @@ const httpLink = new HttpLink({
   },
 });
 
-export default httpLink;
+
+const csrfLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      'x-apollo-operation-name': 'your-operation-name', // Example value
+      'apollo-require-preflight': 'true', // Example value
+    }
+  };
+});
+
+export default httpLink = csrfLink.concat(httpLinkWithCSRF);
+
+
