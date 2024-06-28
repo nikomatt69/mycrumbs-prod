e706f3d033feb6097257daca4939c2ce4755310d
nikomatt69 <nicola.mattioli.95@gmail.com>
2024-06-28 23:52:50 +0200

    82

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,23 +1,6 @@
-const postcss = require('postcss');
-
-const randomClassNames = postcss.plugin('random-class-names', () => {
-  return (root) => {
-    root.walkRules((rule) => {
-      if (rule.selector.startsWith('.')) {
-        const randomName = 'a' + Math.random().toString(36).substring(2, 7);
-        rule.selector = rule.selector.replace(/(\.[\w-]+)/g, `.${randomName}`);
-      }
-    });
-  };
-});
-
 module.exports = {
   plugins: {
-    autoprefixer: {},
-    cssnano: {
-      preset: 'advanced'
-    },
-    'random-class-names': randomClassNames,
-    tailwindcss: {}
+    tailwindcss: {},
+    autoprefixer: {}
   }
 };
